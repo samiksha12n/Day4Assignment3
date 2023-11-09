dbee5353f8017202984f85dc38c245f2d1cdc8de
samiksha12n <samikshaunesarkar@gmail.com>
2023-11-09 15:24:31 +0530

    Using for loop

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Part3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            Console.WriteLine("{0} {1}",a,b);
+            for(int i = 0; i <10; i++) 
+            {
+                c = a + b;
+                Console.WriteLine("{0}",c);
+                a = b;
+                b = c;
+            }
+            Console.ReadKey(); 
+
+        }
+    }
+}
